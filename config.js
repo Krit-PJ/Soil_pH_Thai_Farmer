bdd81de1015bb1e1aadb8aa0379e60a072dd532e
Krit-PJ <phojuang.kitti@gmail.com>
2026-06-27 10:15:02 +0700

    Connect frontend to Apps Script API

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 window.APP_CONFIG = {
   APP_NAME: 'Thai Farm Soil pH Field Index',
   VERSION: '0.1.0',
-  API_URL: 'YOUR_APPS_SCRIPT_WEB_APP_URL',
-  API_TOKEN: 'CHANGE_ME_TOKEN',
+
+  API_URL: 'https://script.google.com/macros/s/AKfycbxwbb-X0ohx4PX0SUeo2DttV3nh2wtOdoQabafCO55CmwQhPJ-icdXekPl3qfGBoWxn/exec',
+  API_TOKEN: 'soil-ph-mvp-001',
+
   DEFAULT_RATIO: '1:2.5',
   SOIL_MASS_G: 20,
   WATER_VOLUME_ML: 50,
